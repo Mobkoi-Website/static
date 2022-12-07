1af5f41d7a2f2aae9b1797cf9fa03419bcbd4b40
Customer <u2407-zzb01crcegyi@guk1002.siteground.eu>
2022-12-07 11:50:19 +0000

    Automated Commit

--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@
 
 echo "Updating..."
 
-find /home/customer/git/static/ \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i 's/https:\/\/stage.wp.mobkoi.com/https:\/\/mobkoistatic.netlify.app/g'
+find /home/customer/git/static/ \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i 's/https:\/\/stage.wp.mobkoi.com/https:\/\/www.mobkoi.com/g'
 
 cd /home/customer/git/static/
 /usr/bin/git pull origin main
