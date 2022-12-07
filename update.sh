60dcffbf8a149e9a0c9b077fe1b0c91bccc539b5
Customer <u2407-zzb01crcegyi@guk1002.siteground.eu>
2022-12-07 11:48:19 +0000

    Automated Commit

--- update.sh
+++ update.sh
@@ -5,6 +5,7 @@ echo "Updating..."
 find /home/customer/git/static/ \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i 's/https:\/\/stage.wp.mobkoi.com/https:\/\/mobkoistatic.netlify.app/g'
 
 cd /home/customer/git/static/
+/usr/bin/git pull origin main
 /usr/bin/git add .
 /usr/bin/git commit -m "Automated Commit"
 /usr/bin/git push origin main --force
